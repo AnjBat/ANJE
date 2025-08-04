c887c4f4aa9b38dafc00860acedd4a0236dadd4a
AnjBat <pumpgaming1@hotmail.com>
2025-08-03 19:28:23 -0500

    Update CrashState.hx

--- source/mikolka/vslice/CrashState.hx
+++ source/mikolka/vslice/CrashState.hx
@@ -32,7 +32,7 @@ class CrashState extends FlxState
 	var isTouchable:Bool = false;
 	#end
 
-	public function new(EMessage:String,callstack:Array<StackItem>)
+	public function new(EMessage:String, callstack:Array<StackItem>)
 	{
 		this.EMessage = EMessage;
 		this.callstack = callstack;
@@ -46,7 +46,7 @@ class CrashState extends FlxState
 		
 		super.create();
 		var previousScreen = new FlxSprite(0, 0, screenBelow);
-		previousScreen.setGraphicSize(FlxG.width,FlxG.height);
+		previousScreen.setGraphicSize(FlxG.width, FlxG.height);
 		previousScreen.updateHitbox();
 
 		textBg = new FlxSprite();
@@ -93,7 +93,7 @@ class CrashState extends FlxState
 						case Method(classname, method):
 							var regex = ~/(([A-Z]+[A-z]*)\.?)+/g;
 							regex.match(classname);
-							line.push("CLS:" + regex.matched(0)+":"+method+"()");
+							line.push("CLS:" + regex.matched(0) + ":" + method + "()");
 						default:
 							Sys.println(stackItem);
 					}
@@ -121,25 +121,21 @@ class CrashState extends FlxState
 		{
 			TitleState.initialized = false;
 			TitleState.closedState = false;
-			// #if LEGACY_PSYCH
-			// if (Main.fpsVar != null) Main.fpsVar.visible = ClientPrefs.showFPS;
-			// if (Main.fpsBg != null) Main.fpsBg.visible = ClientPrefs.showFPS;
-			// #else
 			if (Main.fpsVar != null) Main.fpsVar.visible = ClientPrefs.data.showFPS;
 			if (Main.fpsBg != null) Main.fpsBg.visible = ClientPrefs.data.showFPS;
-			// #end
 			FlxG.sound.pause();
 			FlxTween.globalManager.clear();
 			FlxG.resetGame();
 		}
-		if (FlxG.keys.justPressed.ESCAPE #if TOUCH_CONTROLS_ALLOWED || musicState.touchPad.buttonB.justPressed #end) {
+		if (FlxG.keys.justPressed.ESCAPE #if TOUCH_CONTROLS_ALLOWED || musicState.touchPad.buttonB.justPressed #end)
+		{
 			Sys.exit(1);
 		}
 	}
 
 	function printError(error:CrashData)
 	{
-		printToTrace('H-SLICE ${MainMenuState.pSliceVersion}  (${error.message})');
+		printToTrace('A-SLICE ${MainMenuState.pSliceVersion}  (${error.message})');
 		textNextY += 35;
 		
 		final enter:String = isTouchable ? 'A' : 'ENTER';
@@ -155,7 +151,7 @@ class CrashState extends FlxState
 					case 1:
 						printToTrace(line[0]);
 					case 2:
-						var first_line = line[0].rpad(" ", 33).replace("_","");
+						var first_line = line[0].rpad(" ", 33).replace("_", "");
 						printToTrace('${first_line}${line[1]}');
 					default:
 						printToTrace(" ");
@@ -169,21 +165,20 @@ class CrashState extends FlxState
 					printToTrace(" ");
 				}
 			}
-			// printToTrace('S8:00000000H   RA:80286034H   MM:86A20290H');
 			printSpaceToTrace();
 			printToTrace('RUNTIME INFORMATION');
 			var date_split = error.date.split(" ");
 			printToTrace('TIME:${date_split[1].rpad(" ",9)} DATE:${date_split[0]}');
 			printToTrace('MOD:${error.activeMod.rpad(" ",10)} PE:${MainMenuState.psychEngineVersion.rpad(" ", 5)} SYS:${error.systemName}');
 			printSpaceToTrace();
-			printToTrace('REPORT TO GITHUB.COM/HRK-EXEX/H-SLICE');
+			printToTrace('REPORT TO GITHUB.COM/YOUR_USERNAME/A-SLICE');
 			printToTrace('PRESS $enter TO RESTART / $escape TO EXIT');
 		});
 	}
 
 	static function saveError(error:CrashData)
 	{
-		var errMsg = "H-Slice CRASHED!\n";
+		var errMsg = "A-Slice CRASHED!\n";
 		var dateNow:String = error.date;
 
 		dateNow = dateNow.replace(' ', '_');
@@ -198,13 +193,13 @@ class CrashState extends FlxState
 		errMsg += 'Active mod: ${error.activeMod}\n';
 		errMsg += 'Platform: ${error.systemName}\n';
 		errMsg += '\n';
-		errMsg += '\nPlease report this error to the GitHub page: https://github.com/HRK-EXEX/H-Slice\n\n> Crash Handler written by: sqirra-rng';
+		errMsg += '\nPlease report this error to the GitHub page: https://github.com/AnjBat/A-Slice\n\n> Crash Handler customized by: YOUR_NAME';
 
 		#if !LEGACY_PSYCH
 		@:privateAccess // lazy
-        backend.CrashHandler.saveErrorMessage(errMsg + '\n');
+		backend.CrashHandler.saveErrorMessage(errMsg + '\n');
 		#else
-		var path = './crash/' + 'HSlice_' + dateNow + '.txt';
+		var path = './crash/' + 'ASlice_' + dateNow + '.txt';
 		File.saveContent(path, errMsg + '\n');
 		Sys.println(errMsg);
 		#end
@@ -228,40 +223,13 @@ class CrashState extends FlxState
 	{
 		textNextY += 10;
 	}
-
-	// function styleTest() {
-	// 	printToTrace('THREAD:4  (FLOATING POINT EXCEPTION)');
-	//     printToTrace('PC:8O2B645CH   SR:2OOOFFO3H   VA:FFFFFFFFH');
-	// 	printSpaceToTrace();
-	// 	printToTrace('AT:FFFFOOFFH   VO:00000001H   V1:80000401H');
-	// 	printToTrace('AO:8015A578H   A1:80268300H   A2:412028F6H');
-	// 	printToTrace('A3:43AB25B1H   TO:0000FFOOH   T1:0000FF0OH');
-	// 	printToTrace('T2:00000AAAH   T3:003FFF01H   T4:2000FF01H');
-	// 	printToTrace('T5:00000003H   T6:802DAAAOH   T7:802DASAOH');
-	// 	printToTrace('SO:8010EFC8H   S1:800F7C8CH   S2:8010C924H');
-	// 	printToTrace('S3:80000000H   S4:8010EBBOH   S5:00000000H');
-	// 	printToTrace('S6:00000000H   S7:00000000H   T8:802DA898H');
-	// 	printToTrace('T9:802DAA9SH   GP:00000000H   SP:800AE580H');
-	// 	printToTrace('S8:00000000H   RA:80286034H   MM:86A20290H');
-	// 	printSpaceToTrace();
-	// 	printToTrace('FPCSR:0100080CH');
-	// 	printSpaceToTrace();
-	// 	printToTrace('F00:+7.280E-01 F02:---------  F04:45.458E+02');
-	// 	printToTrace('F06:---------  F08:+1.000E+00 F10:+6.856E-01');
-	// 	printToTrace('F12:+7.280E-01 F14:+5.458E+02 F16:+5.493E+02');
-	// 	printToTrace('F18:+5.458E+02 F20:+0.000E+00 F22:+0.000E+00');
-	// 	printToTrace('F24:+0.000E+00 F26:+0.000E+00 F28:+0.000E+00');
-	// 	printToTrace('F30:+0.000E+00');
-	// }
-	// function name() {
-	// }
 }
 
 typedef CrashData =
 {
 	message:String,
 	trace:Array<Array<String>>,
-	extendedTrace:Array<String>,
+	extendedTrace:Array<String>>,
 	date:String,
 	systemName:String,
 	activeMod:String
