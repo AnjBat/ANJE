ffa2b90a75bacb96349a8438a4393064a4c2a840
AnjBat <pumpgaming1@hotmail.com>
2025-08-04 19:17:34 -0500

    Update OptionsState.hx
    e

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -18,7 +18,7 @@ class OptionsState extends MusicBeatState
 		'Note Colors',
 		'Controls',
 		'Adjust Delay and Combo',
-		'Optimization',
+		'Optimizer',
 		'Graphics',
 		'Visuals',
 		'Gameplay',
@@ -53,7 +53,7 @@ class OptionsState extends MusicBeatState
 				}
 				else
 					openSubState(new options.ControlsSubState());
-			case 'Optimization':
+			case 'Optimizer':
 				openSubState(new options.OptimizeSettingsSubState());
 			case 'Graphics':
 				openSubState(new options.GraphicsSettingsSubState());
